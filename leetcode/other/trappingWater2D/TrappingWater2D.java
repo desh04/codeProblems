3ac9897005ebfc3cea2c912c870070622939737b
desh <singh.desh0404@gmail.com>
2024-05-21 23:25:00 +0530

    tried the lexical order of number i am trying

---
+++ leetcode/other/trappingWater2D/TrappingWater2D.java
@@ -0,0 +1,22 @@
+package leetcode.other.trappingWater2D;
+
+class Solution {
+    public int trappingWater3D(int[][] waterBed) {
+        int waterVolumn = 0;
+
+        int maxL1 = 0, maxL2 = 0;
+        int maxR1 = 0, maxR2 = 0;
+
+        for(int i = 0; i < waterBed.length ; i++) {
+            for(int j = 0; j < waterBed[i].length ; j++) {
+                
+            }
+        } 
+
+        return waterVolumn;
+    }
+}
+
+public class TrappingWater2D {
+
+}
